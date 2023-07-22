d860d767e318f42593b9199c09da406477a07fef
dhanuj <59405402+dhanuj00123@users.noreply.github.com>
2023-07-22 09:59:09 +0000

    initial conmmit

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,29 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
+import Home from "./Home";
 
 function App() {
-  return (
-    <div className="bg-pink-500 text-white w-screen h-screen">
-      <header className="flex flex-col h-full text-center justify-center items-center">
-        <img
-          src={logo}
-          className="animate-spin w-40 h-40 bg-yellow-200 rounded-full"
-          alt="logo"
-        />
-        <p className="mt-8">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ my-portfolio/src/Home/home.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@mui/material";
+
+const Home = () => {
+  return <Button variant="contained">Click Me</Button>;
+};
+export default Home;
